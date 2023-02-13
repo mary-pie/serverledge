3097ab299e32f0b923d2fb948d73ba19837aaf8e
Mary <annerodriguez10@yahoo.it>
2023-02-13 17:09:30 +0100

    check if service is up

--- internal/container/container.go
+++ internal/container/container.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -12,7 +13,7 @@ import (
 	"github.com/grussorusso/serverledge/internal/executor"
 )
 
-//NewContainer creates and starts a new container.
+// NewContainer creates and starts a new container.
 func NewContainer(image, codeTar string, opts *ContainerOptions) (ContainerID, error) {
 	contID, err := cf.Create(image, opts)
 	if err != nil {
@@ -37,6 +38,17 @@ func NewContainer(image, codeTar string, opts *ContainerOptions) (ContainerID, e
 	return contID, nil
 }
 
+// Check if the service requested is up
+func checkServiceUp() bool {
+	_, err := http.Get(fmt.Sprintf("http://localhost:%d", executor.DEFAULT_EXECUTOR_PORT))
+
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+	return true
+}
+
 // Execute interacts with the Executor running in the container to invoke the
 // function through a HTTP request.
 func Execute(contID ContainerID, req *executor.InvocationRequest) (*executor.InvocationResult, time.Duration, error) {
@@ -45,10 +57,28 @@ func Execute(contID ContainerID, req *executor.InvocationRequest) (*executor.Inv
 		return nil, 0, fmt.Errorf("Failed to retrieve IP address for container: %v", err)
 	}
 
+	log.Printf("FUNZIONE EXECUTE, ip addr: %s", ipAddr)
+
 	postBody, _ := json.Marshal(req)
-	postBodyB := bytes.NewBuffer(postBody)
-	resp, waitDuration, err := sendPostRequestWithRetries(fmt.Sprintf("http://%s:%d/invoke", ipAddr,
-		executor.DEFAULT_EXECUTOR_PORT), postBodyB)
+	//postBodyB := bytes.NewBuffer(postBody)
+	log.Printf("postBody %s", postBody)
+	/*resp, waitDuration, err := sendPostRequestWithRetries(fmt.Sprintf("http://%s:%d/invoke", ipAddr,
+	executor.DEFAULT_EXECUTOR_PORT), postBodyB)*/
+
+	//Check if the service is up, before sending request
+	flag := false
+	for !flag {
+		if checkServiceUp() {
+			log.Println("Service up")
+			break
+		} else {
+			log.Println("Service down retry...")
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
+
+	resp, waitDuration, err := sendPostRequest(fmt.Sprintf("http://localhost:%d/invoke",
+		executor.DEFAULT_EXECUTOR_PORT), bytes.NewBuffer(postBody))
 	if err != nil || resp == nil {
 		return nil, waitDuration, fmt.Errorf("Request to executor failed: %v", err)
 	}
@@ -108,5 +138,19 @@ func min(a, b int) int {
 		return a
 	} else {
 		return b
+	}
+}
+
+func sendPostRequest(url string, body io.Reader) (*http.Response, time.Duration, error) {
+	log.Printf("URL: %s", url)
+	log.Printf("BODY: %s", body)
+	var totalWaitMillis = 0
+
+	resp, err := http.Post(url, "application/json", body)
+	if err != nil {
+		log.Println(err)
+		return nil, time.Duration(totalWaitMillis * int(time.Millisecond)), err
+	} else {
+		return resp, time.Duration(totalWaitMillis * int(time.Millisecond)), err
 	}
 }
